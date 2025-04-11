19bd2b6d0e126a64535ec7aea69f74145136b89f
kaua dias <kauaadias2008@gmail.com>
2025-04-11 09:44:27 -0300

    Delete aula09 directory

--- aula09/UsoTernarioString.java
+++
@@ -1,8 +0,0 @@
-package aula09;
-
-public class UsoTernarioString {
-public static void main(String[] args) {
-	String nome1 = "João", nome2 = "Maria"; 
-	System.out.println(nome1.equals(nome2) ? "Igual" : "Diferente");
-	}
-}
